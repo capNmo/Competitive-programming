d5a771492775d542a99c840a33d6437e39bda806
nemo <cap.n3mo@outlook.com>
2023-08-08 14:04:40 +0430

    add MaximumGCD.cpp

---
+++ __MIP_most_important_problems/cf.NumberTheory/MaximumGCD.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <bitset>
+#include <set>
+#include <math.h>
+#include <vector>
+#include <algorithm>
+#include <memory.h>
+#include <unordered_map>
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define nl '\n'
+#define sep "-------------->"
+#define gogogo ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);cout.precision(10);cout.setf(ios::fixed);
+#define all(v) v.begin(), v.end()
+#define int long long
+ 
+void solve() {
+    int n;
+    cin >> n;
+ 
+    cout << n/2 << nl;
+}
+ 
+int32_t main()
+{
+    gogogo
+ 
+    int T;
+    cin >> T;
+ 
+    int i=0;
+    while(i++<T){
+//                cout << "\nTEST CASE [" << i << "]" << nl;
+        solve();
+    }
+ 
+    return 0;
+}
